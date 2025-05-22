271333b65782ec325ea1696732a3c9ff4b87ac38
Neel Dalsania <neel.dalsania@astronomer.io>
2025-05-22 15:59:17 +0530

    Update example_1.py

--- astro-project/dags/example_1.py
+++ astro-project/dags/example_1.py
@@ -5,6 +5,7 @@ from airflow.decorators import dag, task # DAG and task decorators for interfaci
 
 
 
+
 @dag(
     # This defines how often your DAG will run, or the schedule by which your DAG runs. In this case, this DAG
     # will run daily
